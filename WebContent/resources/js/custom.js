e546ebb9dc93edc155220c80683f1c5a451a3aab
Prakash123456780 <gsprakash2192@gmail.com>
2025-12-06 15:05:30 +0530

    Delete WebContent directory

--- WebContent/resources/js/custom.js
+++
@@ -1,8 +0,0 @@
-function disableCustomerID(){
-	if ( $('#requestType').val().toLowerCase() == 'registration' ) {
-        $('#extUserID').prop("readonly", true);
-        $('#extUserID').val('');
-    } else {
-    	$('#extUserID').prop("readonly", false);
-    }
-}
