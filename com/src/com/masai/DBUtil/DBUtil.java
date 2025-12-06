67e603a9e00beb6ab087d8b9c731b83ff2889fc5
Prakash123456780 <gsprakash2192@gmail.com>
2025-12-06 15:09:23 +0530

    Add files via upload

---
+++ com/src/com/masai/DBUtil/DBUtil.java
@@ -0,0 +1,29 @@
+package com.masai.DBUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	
+	public static Connection provideConnection() {
+		
+		Connection conn=null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String url="jdbc:mysql://localhost:3306/cw_project";
+		try {
+			conn= DriverManager.getConnection(url,"root","Soni@123");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;	
+	}
+}
